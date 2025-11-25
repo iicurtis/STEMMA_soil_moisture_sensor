a2f997b3df735aaa732aed70ef1b9b7cbb6e0209
Isaac Curtis <iicurtis@outlook.com>
2025-11-24 20:36:05 -0700

    laziest async modification ever seen

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub enum TemperatureUnit {
 }
 
 #[derive(Copy, Clone, Debug)]
-pub struct SoilSensor<I2C: 'static + Send + Sync, D> {
+pub struct SoilSensor<I2C, D> {
     i2c: I2C,
     // Required for reading without getting any errors
     // https://github.com/adafruit/Adafruit_Seesaw/blob/8728936a5d1a0a7bf2887a82adb0828b70556a45/Adafruit_seesaw.cpp#L737
@@ -37,9 +37,51 @@ pub struct SoilSensor<I2C: 'static + Send + Sync, D> {
 
 impl<I2C, D> SoilSensor<I2C, D>
 where
-    I2C: 'static + Send + Sync,
+    I2C: embedded_hal_async::i2c::I2c,
     D: DelayNs,
 {
+    pub async fn with_units_async(mut self, unit: TemperatureUnit) -> Self {
+        self.unit = unit;
+        self
+    }
+
+    /// Sets the address according to the enabled hardware settings
+    pub async fn with_address_pins_async(mut self, a0: bool, a1: bool) -> Self {
+        self.address = 0x36;
+        if a0 {
+            self.address += 1;
+        }
+        if a1 {
+            self.address += 2;
+        }
+        self
+    }
+
+    /// Sets the address
+    pub async fn with_address_async(mut self, address: u8) -> Self {
+        self.address = address;
+        self
+    }
+
+    /// Sets the reading delay in nanoseconds
+    pub async fn with_delay_async(mut self, temp: u32, moisture: u32) -> Self {
+        self.temp_delay = temp;
+        self.moisture_delay = moisture;
+        self
+    }
+
+    pub async fn with_temperature_delay_async(mut self, temp: u32) -> Self {
+        self.temp_delay = temp;
+        self
+    }
+
+    pub async fn with_moisture_delay_async(mut self, moisture: u32) -> Self {
+        self.moisture_delay = moisture;
+        self
+    }
+}
+
+impl<I2C, D> SoilSensor<I2C, D> {
     pub fn new(i2c: I2C, delay: D) -> Self {
         Self {
             i2c,
@@ -50,7 +92,13 @@ where
             address: 0x36,
         }
     }
+}
 
+impl<I2C, D> SoilSensor<I2C, D>
+where
+    I2C: 'static + Send + Sync,
+    D: DelayNs,
+{
     pub fn with_units(mut self, unit: TemperatureUnit) -> Self {
         self.unit = unit;
         self
@@ -95,6 +143,54 @@ where
 pub struct Reading {
     pub temperature: f32,
     pub moisture: u16,
+}
+
+impl<I2C, D> SoilSensor<I2C, D>
+where
+    I2C: embedded_hal_async::i2c::I2c,
+    D: DelayNs,
+{
+    pub async fn temperature_async(&mut self) -> Result<f32, SoilMoistureSensorError> {
+        let mut buffer = [0; 4];
+        self.i2c_read_async(&[0x00, 0x04], &mut buffer, self.temp_delay)
+            .await?;
+        let raw = i32::from_be_bytes(buffer) as f32;
+        Ok(match self.unit {
+            TemperatureUnit::Celsius => raw * TEMP_C_CONSTANT,
+            TemperatureUnit::Fahrenheit => (raw * TEMP_F_CONSTANT) + TEMP_F_CONSTANT_SUM,
+        })
+    }
+
+    pub async fn moisture_async(&mut self) -> Result<u16, SoilMoistureSensorError> {
+        let mut buffer = [0; 2];
+        self.i2c_read_async(&[0x0F, 0x10], &mut buffer, self.moisture_delay)
+            .await?;
+        Ok(u16::from_be_bytes(buffer))
+    }
+
+    pub async fn read_async(&mut self) -> Result<Reading, SoilMoistureSensorError> {
+        Ok(Reading {
+            temperature: self.temperature_async().await?,
+            moisture: self.moisture_async().await?,
+        })
+    }
+
+    async fn i2c_read_async(
+        &mut self,
+        bytes: &[u8],
+        buffer: &mut [u8],
+        delay_ns: u32,
+    ) -> Result<(), SoilMoistureSensorError> {
+        self.i2c
+            .write(self.address, bytes)
+            .await
+            .map_err(|_| SoilMoistureSensorError::WriteI2CError)?;
+        self.delay.delay_ns(delay_ns);
+        self.i2c
+            .read(self.address, buffer)
+            .await
+            .map_err(|_| SoilMoistureSensorError::ReadI2CError)
+    }
 }
 
 impl<I2C, D> SoilSensor<I2C, D>
